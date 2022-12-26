cafa685c16bebb1863abc949d84c38cd1313cfc6
Federico Roux <federico.roux@globant.com>
2022-12-26 08:30:13 -0300

    extra files deleted

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,16 @@
  */
 
 #include <cassert>
-#include <encoder.hpp>
 #include <iostream>
 
 using namespace std;
 
-/** @brief global instance of encoder */
-encoder enc1{};
-
-/**
- *   @fn adc_log
- *   @brief main program
- */
-
-int adc_log(void) {
-  enc1.log(3);
-  enc1.log(122);
-  enc1.log(493);
-  enc1.log(1022);
-  enc1.log(2033);
-  enc1.log(3001);
-  enc1.log(3900);
-  enc1.log(4095);
-
-  return 0;
-}
 /**
  *   @fn main
  *   @brief main program
  */
 
-int main() {
+int main(int argc, char*argv[]) {
   cout << "Hello C++ Template!" << endl;
-
-  adc_log();
-
-  enc1.calc_velocity();
-  enc1.calc_acceleration();
-
-  enc1.show_positions();
-  enc1.show_velocity();
-  enc1.show_acceleration();
   return 0;
 }
